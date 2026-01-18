74385d540a1c82c22bc69ad6cadc4a4868ace44b
Tejas Jadhav <tejajadhav5733@gmail.com>
2026-01-18 10:32:10 +0530

    Authentication using Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
     <h2>Tejas Jadhav</h2>
-    <Button>Welcome</Button>
+    <Button>Welcome</Button>\
+    <UserButton/>
    </div>
   );
 }
